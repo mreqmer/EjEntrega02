9a98b59f964a8ee6499a4ccd9583273a0955d82c
mreqmer <marta.requejo@iesnervion.es>
2023-10-20 13:46:54 +0200

    terminado

--- EjEntrega02/src4dowhile/ejerciciost204/Ejercicio005.java
+++ EjEntrega02/src4dowhile/ejerciciost204/Ejercicio005.java
@@ -7,32 +7,33 @@ public class Ejercicio005 {
 		// Variables
 		Random rand = new Random();
 		Scanner sc = new Scanner(System.in);
-		final String GANAS = "Ganas";
-		int aleatorio = rand.nextInt(1,101);
+		final String GANAS = "Ganas", MAYOR ="Mayor", MENOR="Menor";
+		int max = 101, min = 1;
+		int random = rand.nextInt(min,max);
 		String prueba;
-		aleatorio = rand.nextInt(1,101);
-		System.out.println(aleatorio);
+		
 		
 		do {
-			
-			System.out.println("Dime si es mayor o menor");
+			random = rand.nextInt(min,max);
+			System.out.println(random);
+			System.out.println("Dime si es Mayor o Menor");
 			prueba = sc.next();
-				switch (prueba) {
-					case "Mayor"->{
-					aleatorio = rand.nextInt(aleatorio,101);
-					System.out.println(aleatorio);
-					}
-					case "Menor"->{
-					aleatorio = rand.nextInt(1,aleatorio);
-					System.out.println(aleatorio);
-					}
-					case "Ganas"->{
-						System.out.println("olee!!");
-						}
+			while(!prueba.equals(GANAS)&& !prueba.equals(MAYOR)&&!prueba.equals(MENOR)){
+				System.err.println("Error");
+				System.out.println("Introduce Mayor, Menor o Ganas:");
+				prueba = sc.next();
 			}
-					
-		}while(!prueba.equals(GANAS));	
-		
+			if (prueba.equals(MENOR)) {
+				max = random ;
+			}else if (prueba.equals(MAYOR)){
+				min = random+1;	
+			}
+		}while(!prueba.equals(GANAS)&& min!=max);
+		if (min==max) {
+		System.out.println("Me trolleaste :(");
+		}else 
+		System.out.println("oleee");
+	
 	sc.close();
 	}
 
--- EjEntrega02/src4dowhile/ejerciciost204/Ejercicio008.java
+++ EjEntrega02/src4dowhile/ejerciciost204/Ejercicio008.java
@@ -14,9 +14,6 @@ public class Ejercicio008 {
 		do {
 		System.out.print("Introduce la primera tirada: ");
 		dado1 = sc.next();
-		
-		System.out.print("Introduce la segunda tirada: ");
-		dado2 = sc.next();
 		//Cacular la suma de los números
 		num1 = switch(dado1) {
 			case "UNO"  -> {
@@ -36,43 +33,45 @@ public class Ejercicio008 {
 			}
 			case "SEIS" ->{
 				yield 6;
-			}
-		}while (num1<1 && num1>6);
-		
-		num2 = switch(dado2) {
-			case "UNO"  -> {
-				yield 1;
-			}
-			case "DOS" -> {
-				yield 2;
-			}
-			case "TRES" ->{
-				yield 3;
-			}	
-			case "CUATRO" ->{
-				yield 4;
-			}	
-			case "CINCO" ->{
-				yield 5;
-			}
-			case "SEIS" ->{
-				yield 6;
-			}
-			default ->{
-			System.out.println("Introduce un número válido");
-				yield -1;
-			}
+			}default->{
+			System.err.println("ERROR.");
+			System.out.println("Introduce el número válido en letras mayúsculas.");
+			yield -1;}
 		};
-
-		if (num1 >=1 && num2 <=6) {
-			suma = num1 + num2;
-			System.out.println("La suma de los dados es: " + suma);
-		}else	{
-			System.err.println("ERROR");
-		}
-	
-		sc.close();
+		}while(num1==-1);
 		
-	}
-
+		do {
+			System.out.print("Introduce la segunda tirada: ");
+			dado2 = sc.next();
+			//Cacular la suma de los números
+			num2 = switch(dado2) {
+				case "UNO"  -> {
+					yield 1;
+				}
+				case "DOS" -> {
+					yield 2;
+				}
+				case "TRES" ->{
+					yield 3;
+				}
+				case "CUATRO" ->{
+					yield 4;
+				}	
+				case "CINCO" ->{
+					yield 5;
+				}
+				case "SEIS" ->{
+					yield 6;
+				}default->{
+				System.err.println("ERROR.");
+				System.out.println("Introduce el número válido en letras mayúsculas.");
+				yield -1;}
+			};
+			}while(num2==-1);
+		suma = num1 + num2;
+		System.out.println("La suma de los dados es: " + suma);
+		sc.close();
+		}
 }
+			
+		
